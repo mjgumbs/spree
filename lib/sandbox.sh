76e25d9f8cd057aaad61e5de78c7a905231edaa7
Michail Gumbs <mjgumbs.200@gmail.com>
2015-11-08 14:14:17 -0400

    changes

--- lib/sandbox.sh
+++ lib/sandbox.sh
@@ -12,7 +12,7 @@ cd ./sandbox
 
 cat <<RUBY >> Gemfile
 gem 'spree', path: '..'
-gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: 'master'
+gem 'spree_auth_devise', github: 'mjgumbs/spree_auth_devise', branch: 'master'
 
 group :test, :development do
   gem 'bullet'
